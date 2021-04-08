e2165fbcaa0db25c80131490639dacdb06768f04
David <davidrfuentes@pm.me>
2021-04-07 21:10:36 -0400

    added historial

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -52,13 +52,13 @@ const leerInput = async (msg) => {
   return desc
 }
 
-const listadoTareasBorrar = async (tareas = []) => {
-  const choices = tareas.map((tarea, i) => {
+const listarLugares = async (lugares = []) => {
+  const choices = lugares.map((lugar, i) => {
     const idx = `${i + 1}.`.green
 
     return {
-      value: tarea.id,
-      name: `${idx} ${tarea.desc}`
+      value: lugar.id,
+      name: `${idx} ${lugar.nombre}`
     }
   })
 
@@ -70,7 +70,7 @@ const listadoTareasBorrar = async (tareas = []) => {
   const preguntas = [{
     type: 'list',
     name: 'id',
-    message: 'Borrar',
+    message: 'Seleccione lugar:',
     choices
   }]
 
@@ -115,7 +115,7 @@ module.exports = {
   inquirerMenu,
   pausa,
   leerInput,
-  listadoTareasBorrar,
+  listarLugares,
   confirmar,
   mostrarListadoChecklist
 }
--- index.js
+++ index.js
@@ -1,24 +1,58 @@
-const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer")
+require('dotenv').config()
+
+const { inquirerMenu, pausa, leerInput, listarLugares } = require('./helpers/inquirer')
+const Busquedas = require('./models/busquedas')
+
+require('colors')
 
 const main = async () => {
-  
-  let opt;
+  let opt
+  let termino; let id = ''
+  let lugares = []
+  let lugarSeleccionado = {}
+  const clima = {}
 
   do {
+    const busquedas = new Busquedas()
     opt = await inquirerMenu()
 
     switch (opt) {
       case 1:
-        console.log('Buscar ciudad')
-        break;
+
+        termino = await leerInput('Ciudad: ')
+        lugares = await busquedas.ciudad(termino)
+
+        id = await listarLugares(lugares)
+
+        if (id === '0') continue
+
+        lugarSeleccionado = lugares.find(l => l.id === id)
+
+        busquedas.agregarHistorial(lugarSeleccionado.nombre)
+
+        const { desc, min, max, temp } = await busquedas.climaLugar(lugarSeleccionado.lat, lugarSeleccionado.lng)
+
+        console.clear()
+        console.log('\nInformación de la ciudad\n'.green)
+        console.log('Ciudad:'.green, lugarSeleccionado.nombre)
+        console.log('Latitud:'.green, lugarSeleccionado.lat)
+        console.log('Longitud:'.green, lugarSeleccionado.lng)
+        console.log('Temperatura:'.green, temp)
+        console.log('Mínima:'.green, min)
+        console.log('Máxima:'.green, max)
+        console.log('Descripción:'.green, desc)
+
+        break
       case 2:
-        console.log('Historial')
+        busquedas.historialCapitalizado.forEach((lugar, i) => {
+          const idx = `${i + 1}.`.green
+          console.log(`${idx} ${lugar}`)
+        })
+        break
     }
 
     if (opt !== 0) await pausa()
-    
   } while (opt !== 0)
-
 }
 
 main()
